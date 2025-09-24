fdc8252ccdba2d761d8c0dbe02b30632a92cea14
AbdullahMustafa745 <abdullahmustafa45678@gmail.com>
2025-09-24 23:36:26 +0300

    Update last.tf

--- last.tf
+++ last.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "web" {
   ami           = "ami-00ca32bbc84273381"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
 
   tags = {
     Name = "sential_check"
